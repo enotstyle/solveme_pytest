eee92a0f5c67ac5f3317d2a5528497fa64652470
Ivan <vanek-te@mail.ru>
2022-08-03 13:50:44 +0700

    add 3.3

--- src/baseclasses/response.py
+++ src/baseclasses/response.py
@@ -6,7 +6,7 @@ class Response:
 
     def __init__(self, response):
         self.response = response
-        self.response_json = response.json()
+        self.response_json = response.json().get('data')
         self.response_status = response.status_code
 
     def validate(self, schema):
@@ -19,7 +19,13 @@ class Response:
 
     def asser_status_code(self, status_code):
         if isinstance(status_code, list):
-            assert self.response_status in status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
+            assert self.response_status in status_code, self
         else:
-            assert self.response_status == status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
+            assert self.response_status == status_code, self
         return self
+
+    def __str__(self):
+        return \
+            f'\nStatus code: {self.response_status} \n' \
+            f'Requested: {self.response.url} \n' \
+            f'Response body: {self.response_json}'
--- tests/test_something.py
+++ tests/test_something.py
@@ -1,26 +1,106 @@
 import requests
 
-from jsonschema import validate
-
 from ..configuration import SERVICE_URL
-
-from ..src.enums.global_enums import GlobalErrorMessages
-# from ..src.schemas.post import POST_SCHEMA
 from ..src.baseclasses.response import Response
-from ..src.pydantic_schemas.post import Post
-
-def test_equal():
-    assert 1 == 1, 'Number is not equal to expected'
-
-def test_is_not_equal():
-    assert 1 != 2, 'Number is equal'
+from ..src.schemas.user import User
 
 
-def test_getting_posts():
-    r = requests.get(url=SERVICE_URL)
-    response = Response(r)
-
-    response.asser_status_code(200).validate(Post) #или POST_SCHEMA
+# def test_equal():
+#     resp = requests.get(url=SERVICE_URL)
+#     print('\n', resp.json())
 
 
-# [{'id': 1, 'title': 'Post 1'}, {'id': 2, 'title': 'Post 2'}, {'id': 3, 'title': 'Post 3'}]
+def test_getting_users_list():
+    response = requests.get(url=SERVICE_URL)
+    test_object = Response(response)
+    test_object.asser_status_code(300).validate(User)
+
+
+
+ # z =  {
+ #     'meta': {
+ #         'pagination': {
+ #             'total': 2749,
+ #             'pages': 275,
+ #             'page': 1,
+ #             'limit': 10,
+ #             'links': {
+ #                 'previous': None,
+ #                 'current': 'https://gorest.co.in/public/v1/users?page=1',
+ #                 'next': 'https://gorest.co.in/public/v1/users?page=2'
+ #             }
+ #         }
+ #     },
+ #     'data': [
+ #         {
+ #             'id': 2781,
+ #             'name': 'Anaadi Mehra',
+ #             'email': 'anaadi_mehra@grant.org',
+ #             'gender': 'female',
+ #             'status': 'inactive'
+ #         },
+ #         {
+ #             'id': 2780,
+ #             'name': 'Mrs. Divjot Pillai',
+ #             'email': 'mrs_pillai_divjot@mann-luettgen.org',
+ #             'gender': 'male',
+ #             'status': 'inactive'
+ #         },
+ #         {
+ #             'id': 2779,
+ #             'name': 'Yogendra Kaur',
+ #             'email': 'kaur_yogendra@yundt.net',
+ #             'gender': 'female',
+ #             'status': 'inactive'
+ #         },
+ #         {
+ #             'id': 2778,
+ #             'name': 'Vrund Guha',
+ #             'email': 'vrund_guha@beahan.biz',
+ #             'gender': 'female',
+ #             'status': 'active'
+ #         },
+ #         {
+ #             'id': 2777,
+ #             'name': 'Miss Brahmdev Mukhopadhyay',
+ #             'email': 'miss_mukhopadhyay_brahmdev@boehm-leuschke.biz',
+ #             'gender': 'female',
+ #             'status': 'inactive'
+ #         },
+ #         {
+ #             'id': 2776,
+ #             'name': 'Bodhan Chattopadhyay',
+ #             'email': 'bodhan_chattopadhyay@hilll.org',
+ #             'gender': 'female',
+ #             'status': 'active'
+ #         },
+ #         {
+ #             'id': 2775,
+ #             'name': 'Bodhan Rana',
+ #             'email': 'rana_bodhan@dubuque-cummerata.net',
+ #             'gender': 'male',
+ #             'status': 'active'
+ #         },
+ #         {
+ #             'id': 2774,
+ #             'name': 'Javas Malik',
+ #             'email': 'malik_javas@thiel.org',
+ #             'gender': 'male',
+ #             'status': 'inactive'
+ #         },
+ #         {
+ #             'id': 2773,
+ #             'name': 'Vidur Gowda III',
+ #             'email': 'iii_gowda_vidur@kulas-ritchie.net',
+ #             'gender': 'male',
+ #             'status': 'inactive'
+ #         },
+ #         {
+ #             'id': 2771,
+ #             'name': 'Param Ahluwalia',
+ #             'email': 'ahluwalia_param@brown.net',
+ #             'gender': 'male',
+ #             'status': 'inactive'
+ #         }
+ #     ]
+ # }
